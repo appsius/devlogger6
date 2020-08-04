0c86108645f3dd9a6299e4220efd7ec86b053a5b
appsius <ilyas.ustunkaya234434@gmail.com>
2020-08-04 14:07:36 +0300

    Bootstrap Setup & Component Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LogFormComponent } from "./components/log-form/log-form.component";
+import { LogsComponent } from './components/logs/logs.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, LogFormComponent, LogsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
