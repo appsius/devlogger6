6b4a80950fc2717a988cbd63b07d3e443aa108b1
appsius <ilyas.ustunkaya234434@gmail.com>
2020-08-04 15:19:39 +0300

    Add, Update & Delete Logs

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -12,16 +12,52 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean = true;
+
   constructor(private logService: LogService) {}
 
   ngOnInit(): void {
     // Subscribe to selectedLog
     this.logService.selectedLog.subscribe((log) => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
+    });
+  }
+
+  onSubmit() {
+    // Check if new
+    if (this.isNew) {
+      // Create new log
+      const newLog = {
+        id: this.generateID(),
+        text: this.text,
+        date: new Date(),
+      };
+      // Add log
+      this.logService.addLog(newLog);
+    } else {
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date(),
+      };
+      // Update log
+      this.logService.updateLog(updLog);
+    }
+  }
+
+  generateID() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (
+      c
+    ) {
+      var r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
     });
   }
 }
